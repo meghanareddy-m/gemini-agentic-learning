39aff8e90dc271a4f703985dde52455d4e3bdf0c
Meghana Reddy <meghanareddy.m13@gmail.com>
2026-08-11 14:38:50 +0530

    Chat Session and History Management

---
+++ 03_chat_session_and_history/client_factory.py
@@ -0,0 +1,22 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+class GeminiClientFactory:
+    def __init__(self):
+        load_dotenv()
+
+    def get_api_key(self)->str:
+        api_key=os.getenv("GEMINI_API_KEY")
+
+        if not api_key:
+            raise ValueError("GEMINI_API_KEY is missing")
+
+        return api_key
+
+    def create_client(self)->genai.Client:
+
+        api_key=self.get_api_key()
+        client = genai.Client(api_key=api_key)
+
+        return client
---
+++ 03_chat_session_and_history/main.py
@@ -0,0 +1,61 @@
+from client_factory import GeminiClientFactory
+from pydantic import BaseModel
+import json
+
+class Message(BaseModel):
+    role:str
+    content:str
+
+history : list[Message] =[]
+
+factory = GeminiClientFactory()
+client = factory.create_client()
+
+def save_history():
+
+    data = []
+
+    for message in history:
+        data.append(message.model_dump())
+
+    with open("conversation_history.json","w",encoding="utf-8") as file:
+        json.dump(data,file,indent=4)
+
+def create_chat():
+
+    return client.chats.create(model="gemini-2.5-flash")
+
+
+def chat_loop(chat):
+    print("Hello!, This is a Gemini Terminal Chat")
+    print("Type 'quit' to exit the chat")
+
+    while True:
+
+        user_input = input("You : ")
+
+        if not user_input.strip():
+            continue
+
+        if user_input.lower() == "quit":
+            save_history()
+            print("History Saved")
+            print("Bye!")
+            break
+
+        history.append(Message(role="user",content=user_input))
+
+        try :
+            response = chat.send_message(user_input)
+            print(f"\nGemini: {response.text}\n")
+            history.append(Message(role="assistant",content=response.text))
+
+        except Exception as e:
+            print(f"Error : {e}")
+
+def main():
+    chat = create_chat()
+    chat_loop(chat)
+
+if __name__ == "__main__":
+    main()
